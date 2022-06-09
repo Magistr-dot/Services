6d7d14cd4698a3f4351019cb1ba8bdb011dfeb28
Alex <nesterov178@gmail.com>
2022-06-09 14:25:46 +0300

    foreground service ready

--- app/src/main/java/com/frigate/services/MainActivity.kt
+++ app/src/main/java/com/frigate/services/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.frigate.services
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
 import com.frigate.services.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,5 +20,33 @@ class MainActivity : AppCompatActivity() {
         binding.bService.setOnClickListener {
             startService(MyService.newIntent(this))
         }
+        binding.bForegroundService.setOnClickListener {
+            showNotification()
+        }
+    }
+
+    private fun showNotification() {
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("Title")
+            .setContentText("Text")
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .build()
+        notificationManager.notify(id++, notification)
+    }
+
+    companion object {
+        private const val CHANNEL_ID = "id channel"
+        private const val CHANNEL_NAME = "name channel"
+        var id = 0
     }
 }
